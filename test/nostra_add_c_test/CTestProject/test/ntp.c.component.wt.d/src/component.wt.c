e0cd2828e8d3b2a59ff0514a6b9530837aedb239
Lukas Reichmann <lukas@reichmann-info.de>
2018-11-17 23:07:44 +0100

    Added more tests, integrated all tests into CTest.

--- test/nostra_add_c_test/CTestProject/test/ntp.c.component.wt.d/src/component.wt.c
+++ test/nostra_add_c_test/CTestProject/test/ntp.c.component.wt.d/src/component.wt.c
@@ -1,12 +1,9 @@
 
 #include "nostra/testproject/component.h"
 
-extern int test2();
-
 int main()
 {
-    test2(); /* from source.c */
-    test();  /* from nostra/testproject/component.h */
+    test(); /* from nostra/testproject/component.h */
 
     return 0;
 }
---
+++ test/nostra_add_cpp_test/CppTestProject/test/ntp.cpp.component.1.wt.d/src/component.1.wt.cpp
@@ -0,0 +1,14 @@
+
+#include "nostra/testproject/component.hpp"
+
+namespace ntp
+{
+int test2();
+}
+
+int main()
+{
+    ntp::test(); /* from nostra/testproject/component.h */
+
+    return 0;
+}
